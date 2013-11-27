a114fa196f768e4317140b190152902c12c573d6
Ivan Efremov <st8998@gmail.com>
2013-11-27 11:34:26 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moments::Application.config.secret_key_base = 'e524a42f37922e4824bd5862a1dd1d744cbebb1b817fff8baac1884ebbf3a0550751b2c08f922fab1ec108e2995b48e2a1ba49dc93b05bb024d2e687bafaf066'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Moments::Application.config.session_store :cookie_store, key: '_moments_session'
