7e5338d2e59796b7a5feb3622a36b3cb77217f09
Ivan Efremov <st8998@gmail.com>
2013-12-07 14:34:39 +0400

    some map experimants

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -6,7 +6,7 @@ $(function() {
 
   var mapOptions = {
     center: homeCoordinates,
-    zoom: 12,
+    zoom: 13,
     mapTypeControl: false,
     panControl: false,
     zoomControl: false,
@@ -60,12 +60,79 @@ $(function() {
     }
   }
 
-  performSearch()
+//  performSearch()
 
-  $(document).on('click', '.search-bar button', performSearch)
-  $(document).on('keyup', '.search-bar', function(e) {
-    if (e.which == 13)
-      performSearch()
-  })
+//  $(document).on('click', '.search-bar button', performSearch)
+//  $(document).on('keyup', '.search-bar', function(e) {
+//    if (e.which == 13)
+//      performSearch()
+//  })
+
+
+  var geocoder;
+  var map;
+  var infowindow = new google.maps.InfoWindow();
+  var marker;
+  function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(40.730885,-73.997383);
+    var mapOptions = {
+      zoom: 8,
+      center: latlng
+    }
+    map = new google.maps.Map($('#main-map').get(0), mapOptions);
+  }
+
+//  initialize()
+//  codeLatLng()
+
+  function codeLatLng() {
+//    var input = document.getElementById("latlng").value;
+//    var latlngStr = input.split(",",2);
+//    var lat = parseFloat(latlngStr[0]);
+//    var lng = parseFloat(latlngStr[1]);
+    var latlng = new google.maps.LatLng(53.20346531907318, 50.14474779367447);
+    geocoder.geocode({'latLng': latlng, language: 'ru'}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        console.log(results)
+
+        if (results[1]) {
+          map.setZoom(11);
+          marker = new google.maps.Marker({
+            position: latlng,
+            map: map
+          });
+          infowindow.setContent(results[0].formatted_address);
+          infowindow.open(map, marker);
+        }
+      } else {
+        alert("Geocoder failed due to: " + status);
+      }
+    });
+  }
+
+
+  var geocoder = new google.maps.Geocoder()
+  setTimeout(function() {
+
+//    var service = new google.maps.places.AutocompleteService();
+//    service.getQueryPredictions({ input: 'аврора 57'}, function(data) {
+//      console.log(data)
+//    });
+
+    geocoder.geocode({
+      'address': 'собор святого вита'}, function(results, status) {
+      console.log(results)
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+        });
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    })
+  }, 500)
 
 });
