fff6e26e38ab4ce03ce9179746760363772dfa01
Ivan Efremov <st8998@gmail.com>
2013-12-23 21:02:03 +0400

    pictures line placeholder

--- app/assets/javascripts/comp/pictures/pictures_line.js.jsx
+++ app/assets/javascripts/comp/pictures/pictures_line.js.jsx
@@ -17,7 +17,7 @@ define('comp/pictures/pictures_line', ['models/picture', 'comp/pictures/picture'
     },
 
     render: function() {
-      var pics = this.props.pictures
+      var pics = this.props.pictures, placeholder
 
       Picture.fitInRow(pics, this.props.maxWidth, this.props.maxHeight+2)
       Picture.enhanceInRow(pics, this.props.maxWidth, this.props.enhanceRatio)
@@ -25,9 +25,13 @@ define('comp/pictures/pictures_line', ['models/picture', 'comp/pictures/picture'
       var pictures = _.map(pics, function(pic) { return this.props.thumbComponent({picture: pic})}.bind(this))
       var lineHeight = pics[0] ? pics[0].thHeight : this.props.maxHeight
 
+      if (!pictures.length)
+        placeholder = <h3 className='placeholder'>Никто пока ничего не загружал</h3>
+
       return (
         <div className='pictures-line-component'>
           <ul style={{height: lineHeight}}>{pictures}</ul>
+          {placeholder}
         </div>
       )
     }
