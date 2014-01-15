3bf1c59688d592e9d32b250042db272bb8b38c73
Ivan Efremov <st8998@gmail.com>
2014-01-16 00:09:33 +0400

    configure demo account in before_action

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,5 +1,11 @@
 class BlogController < ApplicationController
 
+  before_action do
+    if !(cookies[:dakey] && Account.exists?(key: cookies[:dakey]))
+      cookies.permanent[:dakey] = {value: Account.create_demo.key, path: '/blog'}
+    end
+  end
+
   def article
     render "blog/#{params[:article_key]}"
   end
