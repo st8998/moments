8acc7187232a8f7ebc27bec39794d68de1d272da
Ivan Efremov <st8998@gmail.com>
2014-02-28 13:47:46 +0400

    slim config bump ruby version

---
+++ config/initializers/slim.rb
@@ -0,0 +1,4 @@
+# Tell slim to ignore angularjs expressions in templates like this:
+# p {{some.value}}
+# https://github.com/slim-template/slim/pull/434
+Slim::Engine.set_default_options :attr_delims => {'(' => ')', '[' => ']'}
