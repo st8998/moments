fb87061593730d1d9925815950343b5a3bf53ca4
Ivan Efremov <st8998@gmail.com>
2014-03-02 11:30:20 +0400

    conditional get headers added to templates_controller

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -5,6 +5,10 @@ class TemplatesController < ApplicationController
   layout false
 
   def template
-    render params.require(:path)
-  end
+    template = lookup_context.find_template(params[:path])
+
+    if stale?(last_modified: template.updated_at.utc)
+      render params.require(:path)
+    end
+ end
 end
--- test/controllers/templates_controller_test.rb
+++ test/controllers/templates_controller_test.rb
@@ -1,9 +1,27 @@
 require 'test_helper'
 
 class TemplatesControllerTest < ActionController::TestCase
+  setup do
+    TemplatesController.send(:prepend_view_path, data_root)
+    @test_template = File.new("#{data_root}/test_template.html.slim")
+  end
+
   test 'accepts pictures upload' do
-    get :template, path: '/directives/gallery.html.slim'
+    get :template, path: '/test_template.html.slim'
 
     assert_response :success
+    assert_equal @test_template.mtime.httpdate, response.header['Last-Modified']
+  end
+
+  test 'correct cache headers' do
+    @request.env['HTTP_IF_MODIFIED_SINCE'] = @test_template.mtime.httpdate
+    get :template, path: '/test_template.html.slim'
+
+    assert_response 304
+
+    @request.env['HTTP_IF_MODIFIED_SINCE'] = 1.minute.ago(@test_template.mtime).httpdate
+    get :template, path: '/test_template.html.slim'
+
+    assert_response 200
   end
 end
