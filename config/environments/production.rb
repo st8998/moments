a4321580c8b50fbaf95a1fa54e40dfbc272b6b19
Ivan Efremov <st8998@gmail.com>
2014-03-05 17:06:23 +0400

    more configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,10 @@ Moments::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w(  )
+  config.assets.precompile +=
+      Dir[::Rails.root.join('app/assets/*/*.{js,css,sass,scss}*')].
+          map { |i| File.basename(i).sub(/(\.\w+)?(\.\w+)*$/, '\1') }.
+          reject { |i| i =~ /^application\.(js|css)$/ }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
