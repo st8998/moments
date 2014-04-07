5eb993618b0d2477994295fd8d8e74d334d6906a
Ivan Efremov <st8998@gmail.com>
2014-04-07 19:38:46 +0400

    cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Moments::Application.routes.draw do
   root to: 'application#root'
 
-  get '/blog/:article_key', to: 'blog#article'
-
   get :login, to: 'sessions#new'
   post :login, to: 'sessions#create'
   delete :logout, to: 'sessions#destroy'
@@ -10,23 +8,8 @@ Moments::Application.routes.draw do
   get '/template/*path', to: 'templates#template'
 
   scope path: '/:account_key' do
-
-    get '/', to: 'photostream#index', as: :account_root
-
     scope defaults: {format: :json} do
-      get '/photostream', to: 'photostream#index'
-      post '/photostream/:picture_id', to: 'photostream#add'
-      delete '/photostream/:picture_id', to: 'photostream#remove'
 
-      #scope path: '/:pictures_set_id' do
-      #  resources :pictures, only: [:index]
-      #
-      #  post '/reorder', to: 'pictures#reorder'
-      #end
-
-      resources :pictures do
-        post :upload, on: :collection
-      end
     end
   end
 end
