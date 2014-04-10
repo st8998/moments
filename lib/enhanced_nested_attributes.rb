ddc3d0ba237867a65d9c239c8ab2222310c32632
Ivan Efremov <st8998@gmail.com>
2014-04-10 13:03:08 +0400

    quick fix for enhanced nested attributes in case of has_many

--- lib/enhanced_nested_attributes.rb
+++ lib/enhanced_nested_attributes.rb
@@ -5,20 +5,33 @@ module EnhancedNestedAttributes
     attr_names.extract_options!
 
     attr_names.each do |name|
-      class_name = self.reflect_on_association(name).class_name
+      association = self.reflect_on_association(name)
+      class_name = association.class_name
 
-      class_eval <<-RUBY, __FILE__, __LINE__+1
-        def #{name}= attrs
-          if attrs.is_a?(Hash) && attrs[:id]
-            self.#{name} = #{class_name}.find(attrs[:id])
-            self.#{name}_attributes = attrs
-          elsif attrs.is_a?(#{class_name})
-            super
-          else
-            self.#{name}_attributes = attrs
+      if association.macro == :has_one
+        class_eval <<-RUBY, __FILE__, __LINE__+1
+          def #{name}= attrs
+            if attrs.is_a?(Hash) && attrs[:id]
+              self.#{name} = #{class_name}.find(attrs[:id])
+              self.#{name}_attributes = attrs
+            elsif attrs.is_a?(#{class_name})
+              super
+            else
+              self.#{name}_attributes = attrs
+            end
           end
-        end
-      RUBY
+        RUBY
+      else
+        class_eval <<-RUBY, __FILE__, __LINE__+1
+          def #{name}= attrs
+            if attrs.first && attrs.first.is_a?(#{class_name})
+              super
+            else
+              self.#{name}_attributes = attrs
+            end
+          end
+        RUBY
+      end
     end
   end
 end
