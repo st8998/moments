5a815c21d9314493e9ce9d473219bf7b6d340c27
Ivan Efremov <st8998@gmail.com>
2014-06-10 12:54:15 +0400

    some styling

--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -1,25 +1,14 @@
 class PhotoSerializer < ActiveModel::Serializer
-  attributes(:id, :description)
+  attributes(:id, :description, :width, :height)
 
-  attributes(:image_url_small, :image_url_normal, :image_url_big)
-
-  def width
-    object.image_width
-  end
-
-  def height
-    object.image_height
-  end
-
-  def image_url_small
-    object.image.thumb('512x512>').url if object.image
-  end
-
-  def image_url_normal
-    object.image.thumb('1024x1024>').url if object.image
-  end
-
-  def image_url_big
-    object.image.thumb('2048x2048>').url if object.image
+  def attributes
+    super.tap do |attrs|
+      if object.image
+        attrs[:image_url_square] = object.image.thumb('200x200#').convert('-sharpen 0x.75').url
+        attrs[:image_url_small] = object.image.thumb('512x512>').convert('-sharpen 0x.75').url
+        attrs[:image_url_normal] = object.image.thumb('1024x1024>').convert('-sharpen 0x.5').url
+        attrs[:image_url_big] = object.image.thumb('2048x2048>').url
+      end
+    end
   end
 end
