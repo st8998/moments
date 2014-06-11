0d81e9ddf6d543e7bab1de153830147a7dd1a7a4
Ivan Efremov <st8998@gmail.com>
2014-06-11 20:10:07 +0400

    format dates with moment.js

---
+++ app/assets/javascripts/directives/formatted_date.js
@@ -0,0 +1,63 @@
+angular.module('app').directive('mFormattedDate', function($moment, $timeout) {
+  return {
+    restrict: 'A',
+    link: function(scope, elem, attrs) {
+      var date, timer
+
+      attrs.$observe('mFormattedDate', function(attrDate) {
+        date = $moment(attrDate)
+        $timeout.cancel(timer)
+        outputDate()
+      })
+
+      function outputDate() {
+        if (date.isValid()) {
+          var diff = moment.duration(moment().diff(date))
+          var text
+
+          if (diff.days() > 0) {
+            text = date.format('DD MMM в HH:MM')
+          } else if (diff.hours() > 0) {
+            text = date.fromNow()
+            timer = $timeout(outputDate, 60*60*1000)
+          } else if (diff.minutes() > 0 || diff.seconds() > 45) {
+            text = date.fromNow()
+            timer = $timeout(outputDate, 60*1000)
+          } else {
+            text = date.fromNow()
+            timer = $timeout(outputDate, (45-diff.seconds())*1000)
+          }
+
+          elem.text(text)
+        }
+      }
+
+      elem.on('$destroy', function() {
+        $timeout.cancel(timer)
+      })
+    }
+  }
+})
+
+angular.module('app').filter('formatDate', function($moment) {
+  return function(date, type) {
+    console.log('FORMAT', date, type)
+
+    if (!date) return
+
+    var date = $moment(date)
+
+    if (date.isValid()) {
+      switch (type) {
+        case 'pretty':
+          if (moment.duration(moment().diff(date)).days() == 0) {
+            return date.fromNow()
+          } else {
+            return date.format('DD MMM в HH:MM')
+          }
+        default:
+          return date.format(type)
+      }
+    }
+  }
+})
