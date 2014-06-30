6019fee776f61be0faa618750e9ed24ec112c1bc
Ivan Efremov <st8998@gmail.com>
2014-07-01 01:05:02 +0400

    photos dnd reordering

---
+++ app/assets/javascripts/directives/reorder.js
@@ -0,0 +1,55 @@
+angular.module('app').directive('mReorder', function($parse) {
+  return {
+    restrict: 'A',
+    link: function(scope, root, attrs) {
+      var onReorder = attrs['onReorder'] ? $parse(attrs['onReorder']) : null
+
+      var active, items
+
+      root.on('dragstart', '[draggable=true]', function(e) {
+        active = this
+        items = root.find('[draggable=true]')
+
+        items.each(function() {
+          var el = $(this)
+          el.data('mReorderDims', {left: parseInt(el.css('left')), width: parseInt(el.css('width'))})
+        })
+
+        active.style.opacity = '0'
+        e.originalEvent.dataTransfer.effectAllowed = 'move'
+        e.originalEvent.dataTransfer.setData('text/html', this.innerHTML)
+      })
+
+      // drag enter/leave styling
+      root.on('dragenter', '[draggable=true]', function() {
+        var el = $(this), dims = el.data('mReorderDims')
+        el.css({left: dims.left + 5, width: dims.width - 5})
+      }).on('dragleave drop', '[draggable=true]', function() {
+        var el = $(this)
+        el.css(el.data('mReorderDims'))
+      })
+
+      root.on('dragend', '[draggable=true]', function() {
+        active.style.opacity = '1'
+      })
+
+      root.on('dragover', '[draggable=true]', function(e) {
+        e.preventDefault()
+        e.originalEvent.dataTransfer.dropEffect = 'move'
+      })
+
+      root.on('drop', '[draggable=true]', function(e) {
+        e.stopPropagation()
+
+        var from = items.index(active),
+          to = items.index(this)
+
+        if (onReorder) {
+          scope.$apply(function() {
+            onReorder(scope, {from: from, to: to})
+          })
+        }
+      })
+    }
+  }
+})
--- app/assets/javascripts/widgets/moment_widget.js
+++ app/assets/javascripts/widgets/moment_widget.js
@@ -40,36 +40,30 @@ angular.module('app').directive('momentWidget', function($http, Moment, api, $ro
           $http.delete(api('/photos/', photo.id))
           _.remove(scope.moment.photos, {id: photo.id})
         },
+        movePhoto: function(from, to) {
+          var photos = scope.moment.photos, max, min
+
+          if (from > to) {
+            max = from
+            min = to
+          } else {
+            max = to
+            min = from
+          }
+
+          var photo = photos.splice(max, 1)[0]
+          photos.splice(min, 0, photo)
+
+          _.each(photos, function(photo, index) {
+            photo.position = index
+          })
+        },
 
         submit: function() {
           scope.close()
           scope.onSubmit()
         }
       })
-
-//      $scope.createMoment = function() {
-//        if (!$scope.newMoment.date) $scope.newMoment.date = new Date()
-//
-//        $http.post(api('moments'), {moment: $scope.newMoment.attributes()}).success(function(moment) {
-//          $scope.moments.unshift(new Moment(moment))
-//          $scope.closeMoment($scope.newMoment)
-//          $scope.newMoment = new Moment()
-//        })
-//      }
-//
-//      $scope.updateMoment = function(moment) {
-//        $http.put(api('moments', moment.id), {moment: moment.attributes()}).success(function(attrs) {
-//          $scope.closeMoment(moment)
-//          moment.assignAttributes(attrs)
-//        })
-//      }
-//
-//      $scope.removeMoment = function(moment) {
-//        _.remove($scope.moments, {id: moment.id})
-//        $http.delete(api('moments', moment.id))
-//      }
-
-
     }
   }
 
