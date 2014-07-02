864b874f4a20d149a7eacd5d5f12d8b83dfe941e
Ivan Efremov <st8998@gmail.com>
2014-07-02 20:19:15 +0400

    cleanup api service (remove all the shit)

--- app/assets/javascripts/services/api.js
+++ app/assets/javascripts/services/api.js
@@ -1,10 +1,4 @@
-angular.module('app').provider('api', function() {
-  var demo = false
-
-  this.demoMode = function(demoMode) {
-    demo = demoMode
-  }
-
+angular.module('app').factory('api', function() {
   function buildApi(key) {
     function routeBuilder() {
       return ('/' + key + '/' + Array.prototype.join.call(arguments, '/')).replace(/\/\//g, '/')
@@ -15,13 +9,5 @@ angular.module('app').provider('api', function() {
     return routeBuilder
   }
 
-  this.$get = ['$http', 'cookies', function($http, cookies) {
-    // TODO think about failure scenario, no akey cookie for example
-    // not demo api should have akey cookies setup in any cases
-    if (demo) {
-      return buildApi(cookies.get('dakey'))
-    } else {
-      return buildApi($('meta[name=akey]').attr('content'))
-    }
-  }]
+  return buildApi($('meta[name=akey]').attr('content'))
 })
