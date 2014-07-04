449e7ff8ed70b85fe38b6ec2b3a895d07659dd97
Ivan Efremov <st8998@gmail.com>
2014-07-04 13:29:03 +0400

    add some tests for fulltext searching

--- test/controllers/places_controller_test.rb
+++ test/controllers/places_controller_test.rb
@@ -8,14 +8,26 @@ class PlacesControllerTest < ActionController::TestCase
     @p1 = create(:place, name: 'Name1')
     @p2 = create(:place, name: 'Name2')
     @p3 = create(:place, name: 'Name3')
+    @p4 = create(:place, name: 'Nona1')
   end
 
   test 'exact search is working' do
-    skip
     get :search, term: 'Name1', account_key: accounts(:st8998).key, format: :json
 
     assert_equal 1, response_json.size
     assert_equal 'Name1', response_json.first['name']
+  end
+
+  test 'part name search is working' do
+    get :search, term: 'Na', account_key: accounts(:st8998).key, format: :json
+
+    assert_equal 3, response_json.size
+  end
+
+  test 'searching with errors' do
+    get :search, term: 'Nmae', account_key: accounts(:st8998).key, format: :json
+
+    assert_equal 3, response_json.size
   end
 
   def response_json
